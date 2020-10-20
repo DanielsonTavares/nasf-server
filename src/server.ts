09c9bbf9d723c2a4ee667ac8c920ab2c3a03a58f
danielson <caosdan@gmail.com>
2020-10-19 23:11:28 -0300

    Configurando TypeScript e Express

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+const port = 3333;
+
+app.get('/', (req, res)=>{
+    res.json({message: 'hello world'})
+})
+
+
+app.listen(port, ()=>{
+    console.log(`listening on port ${port}`);
+    
+})
