72da3b64576387944536fb5bc93077e7c6e1f329
danielson <caosdan@gmail.com>
2020-10-21 11:31:35 -0300

    configurando as variaveis de ambiente

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
+import dotenv from 'dotenv';
 import express from 'express';
 
 import usuarioRotas from './routers/usuarioRoute';
 import grupoRotas from './routers/grupoRoute';
 
+dotenv.config();
 const app = express();
-const port = 3333;
+const port = process.env.PORT;
 
 app.use(usuarioRotas);
 app.use(grupoRotas);
