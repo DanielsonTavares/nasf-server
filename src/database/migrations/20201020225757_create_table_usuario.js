d267ce01083b70ab01ac59fe246df68514c49067
danielson <caosdan@gmail.com>
2020-10-23 16:06:15 -0300

    atualizando migrations Complementando a tabela usuario

--- src/database/migrations/20201020225757_create_table_usuario.js
+++ src/database/migrations/20201020225757_create_table_usuario.js
@@ -1,6 +1,11 @@
 exports.up = (knex) => knex.schema.createTable('usuario', (table) => {
-  table.increments('id');
-  table.text('nome').unique().notNullable();
+  table.increments('id').comment('Identificador do usuario');
+  table.string('nome', 100).comment('Nome completo do usuario');
+  table.string('login', 100).unique().notNullable().comment('Login de acesso do usuario');
+  table.string('senha', 100).notNullable().comment('Senha do usuário');
+  table.string('email', 100).notNullable().comment('Email do usuario');
+  table.datetime('data_criacao').defaultTo(knex.fn.now(6));
+  table.datetime('data_atualizacao');
 });
 
 exports.down = (knex) => knex.schema.dropTable('usuario');
--- src/database/seeds/001_usuario.js
+++ src/database/seeds/001_usuario.js
@@ -2,6 +2,13 @@ exports.seed = (knex) =>
   // Deletes ALL existing entries
   knex('usuario').del()
     .then(() => knex('usuario').insert([
-      { nome: 'danielson de oliveira tavares' },
-      { nome: 'vanessa de araujo teixeira tavares' },
+      {
+        nome: 'danielson de oliveira tavares', login: 'dan', senha: '123456', email: 'dan@gmail.com',
+      },
+      {
+        nome: 'vanessa de araujo teixeira tavares', login: 'vanessa', senha: 'van123', email: 'vanessa@gmail.com',
+      },
+      {
+        nome: 'administrador', login: 'admin', senha: 'admin', email: 'admin@email.com',
+      },
     ]));
