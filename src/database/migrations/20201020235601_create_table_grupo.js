2f0e29f28a65a76297cb4356efac16515185e913
danielson <caosdan@gmail.com>
2020-10-23 20:48:23 -0300

    Atualizando migrations Complementando a tabela grupo

--- src/database/migrations/20201020235601_create_table_grupo.js
+++ src/database/migrations/20201020235601_create_table_grupo.js
@@ -1,7 +1,9 @@
 exports.up = (knex) => knex.schema.createTable('grupo', (table) => {
-  table.increments('id');
-  table.text('nome').unique().notNullable();
-  table.text('descricao');
+  table.increments('id').comment('Identificador do grupo');
+  table.string('nome', 100).unique().notNullable().comment('Nome do grupo');
+  table.string('descricao').comment('Descrição do grupo');
+  table.datetime('data_criacao').defaultTo(knex.fn.now(6));
+  table.datetime('data_atualizacao');
 });
 
 exports.down = (knex) => knex.schema.dropTable('grupo');
