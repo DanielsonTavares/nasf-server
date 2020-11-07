78f203edec2c3c5666133479a6492cb3f49d9b94
danielson <caosdan@gmail.com>
2020-11-07 01:18:14 -0300

    Test integração usuarioController

--- __tests__/integration/usuario.test.ts
+++ __tests__/integration/usuario.test.ts
@@ -2,7 +2,6 @@
 
 import request from 'supertest';
 import app from '../../src/app';
-import UsuarioModel from '../../src/models/usuarioModel';
 
 import knex from '../../src/database';
 
@@ -20,59 +19,254 @@ describe('UsuarioModel', () => {
       senha: '01',
     };
 
-    const usuario = new UsuarioModel(usu);
-    const result = await usuario.create();
+    const response = await request(app)
+      .post('/usuario')
+      .send(usu);
 
-    expect(result.login).toBe('usuario01');
+    expect(response.status).toBe(201);
+    expect(response.body).toBeTruthy();
 
     done();
   });
 
-  it('Deve atualizar a senha do usuario01 para www', async (done) => {
+  it('Deve criticar login nulo ao inserir um usuario', async (done) => {
+    const usu = {
+      email: 'usuario01@email.com',
+      login: '',
+      nome: 'usuario zero um',
+      senha: '01',
+    };
+
+    const response = await request(app)
+      .post('/usuario')
+      .send(usu);
+
+    expect(response.status).toBe(400);
+    expect(response.body.message).toBe('Login não pode ser nulo');
+
+    done();
+  });
+
+  it('Não deve inserir um usuario já existente', async (done) => {
     const usu = {
       email: 'usuario01@email.com',
       login: 'usuario01',
       nome: 'usuario zero um',
-      senha: 'www',
+      senha: '01',
     };
 
-    const usuario = new UsuarioModel(usu);
-    const result = await usuario.update();
+    const response = await request(app)
+      .post('/usuario')
+      .send(usu);
 
-    expect(result.senha).toBe('www');
+    expect(response.status).toBe(500);
+
+    done();
+  });
+
+  // it('Deve atualizar a senha do usuario01 para www utilizando o login', async (done) => {
+  //   const usu = {
+  //     email: 'usuario01@email.com',
+  //     login: 'usuario01',
+  //     nome: 'usuario zero um',
+  //     senha: 'www',
+  //   };
+
+  //   const response = await request(app)
+  //     .put('/usuario')
+  //     .send(usu);
+
+  //   expect(response.status).toBe(201);
+  //   expect(response.body.ok).toBe(true);
+
+  //   done();
+  // });
+
+  it('Deve atualizar a senha do usuario01 para qqq utilizando o id', async (done) => {
+    const usu = {
+      id: 1,
+      email: 'usuario01@email.com',
+      login: 'usuario01',
+      nome: 'usuario zero um',
+      senha: 'qqq',
+    };
+
+    const response = await request(app)
+      .put('/usuario')
+      .send(usu);
+
+    expect(response.status).toBe(201);
+
+    done();
+  });
+
+  it('Deve enviar status 404 ao tentar atualizar um usuario inexistente', async (done) => {
+    const usu = {
+      id: 99,
+      email: 'naoexiste@email.com',
+      login: 'usuario01',
+      nome: 'usuario zero um',
+      senha: 'qqq',
+    };
+
+    const response = await request(app)
+      .put('/usuario')
+      .send(usu);
+
+    expect(response.status).toBe(404);
+
+    done();
+  });
+
+  it('Deve criticar ao tentar atualizar um usuario sem id', async (done) => {
+    const usu = {
+      email: 'naoexiste@email.com',
+      login: 'usuario01',
+      nome: 'usuario zero um',
+      senha: 'qqq',
+    };
+
+    const response = await request(app)
+      .put('/usuario')
+      .send(usu);
+
+    expect(response.status).toBe(500);
+
+    done();
+  });
+
+  it('Deve deletar um usuario', async (done) => {
+    const usu = {
+      id: 1,
+    };
+
+    const response = await request(app)
+      .delete('/usuario')
+      .send(usu);
+
+    expect(response.status).toBe(200);
+    expect(response.body.message).toBe('exclusão realizada com sucesso');
+
+    done();
+  });
+
+  it('Deve criticar usuario inexistente ao deletar', async (done) => {
+    const usu = {
+      id: 99,
+    };
+
+    const response = await request(app)
+      .delete('/usuario')
+      .send(usu);
+
+    expect(response.status).toBe(404);
+    expect(response.body.message).toBe('Não foi encontrado registro para o id');
 
     done();
   });
 
   it('Deve recuperar uma lista de usuários', async (done) => {
-    const result = await UsuarioModel.findAll();
+    const usu01 = {
+      email: 'usuario01@email.com',
+      login: 'usuario01',
+      nome: 'usuario zero um',
+      senha: '01',
+    };
 
-    expect(result.length).toBeGreaterThan(1);
+    const usu02 = {
+      email: 'usuario02@email.com',
+      login: 'usuario02',
+      nome: 'usuario zero dois',
+      senha: '02',
+    };
 
-    done();
-  });
+    await request(app)
+      .post('/usuario')
+      .send(usu01);
 
-  it('Deve encontrar um usuário com ID 20', async (done) => {
-    const result = await UsuarioModel.findOne('20');
-    expect(result).toBe(true);
+    await request(app)
+      .post('/usuario')
+      .send(usu02);
 
-    done();
-  });
-
-  it('Não deve encontrar um usuário com ID 77', async (done) => {
-    const result = await UsuarioModel.findOne('77');
-    expect(result).toBe(false);
-
-    done();
-  });
-
-  it('Deve recuperar um usuário com ID 20', async (done) => {
-    // const result = await UsuarioModel.findById('20');
-    // expect(result[0].login).toBe('dan');
-
-    const response = await request(app).get('/usuario/20');
+    const response = await request(app)
+      .get('/usuario');
 
     expect(response.status).toBe(200);
+    expect(response.body.result.length).toBeGreaterThan(1);
+
+    done();
+  });
+
+  it('Deve recuperar um usuário pelo id', async (done) => {
+    const response = await request(app)
+      .get('/usuario/3');
+
+    expect(response.status).toBe(200);
+    expect(response.body.length).toEqual(1);
+
+    done();
+  });
+
+  it('Deve criticar ao tentar recuperar um usuário com id inexistente', async (done) => {
+    const response = await request(app)
+      .get('/usuario/99');
+
+    expect(response.status).toBe(404);
+    expect(response.body.message).toBe('Não foi encontrado registro para o id');
+
+    done();
+  });
+
+  it('Deve logar usuario informando login/senha', async (done) => {
+    const usu = {
+      email: 'usuario01@email.com',
+      login: 'usuario01',
+      nome: 'usuario zero um',
+      senha: '01',
+    };
+
+    const response = await request(app)
+      .post('/usuario/login')
+      .send(usu);
+
+    expect(response.status).toBe(200);
+    expect(response.body.message).toBe('usuario01 logado com sucesso');
+
+    done();
+  });
+
+  it('Deve criticar login nulo ao logar um usuario', async (done) => {
+    const usu = {
+      email: 'usuario01@email.com',
+      login: '',
+      nome: 'usuario zero um',
+      senha: '01',
+    };
+
+    const response = await request(app)
+      .post('/usuario/login')
+      .send(usu);
+
+    expect(response.status).toBe(400);
+    expect(response.body.message).toBe('Login não pode ser nulo');
+
+    done();
+  });
+
+  it('Não deve logar usuario informando login/senha inválidos', async (done) => {
+    const usu = {
+      email: 'usuario01@email.com',
+      login: 'usuario01',
+      nome: 'usuario zero um',
+      senha: '01as',
+    };
+
+    const response = await request(app)
+      .post('/usuario/login')
+      .send(usu);
+
+    expect(response.status).toBe(401);
+    expect(response.body.message).toBe('usuario01 nao logado');
 
     done();
   });
