8eb73d5d2e9bc90832871020faaf6c6cbaf46f0b
danielson <caosdan@gmail.com>
2020-11-06 23:33:11 -0300

    Configurando o jest
    Configurando o coverage.
    Colocando tratamento para que o teste encerre ao final.
    Criação de script para criar e apagar o banco de teste

--- __tests__/integration/usuario.test.ts
+++ __tests__/integration/usuario.test.ts
@@ -4,22 +4,15 @@ import request from 'supertest';
 import app from '../../src/app';
 import UsuarioModel from '../../src/models/usuarioModel';
 
-const limpa = async () => {
-  const usu01 = {
-    email: 'usuario01@email.com',
-    login: 'usuario01',
-    nome: 'usuario zero um',
-    senha: '01',
-  };
-
-  const usuario01 = new UsuarioModel(usu01);
-  await usuario01.delete();
-};
-
-limpa();
+import knex from '../../src/database';
 
 describe('UsuarioModel', () => {
-  it('Deve inserir um usuario com login usuario01', async () => {
+  afterAll(async (done) => {
+    await knex.destroy();
+    done();
+  });
+
+  it('Deve inserir um usuario com login usuario01', async (done) => {
     const usu = {
       email: 'usuario01@email.com',
       login: 'usuario01',
@@ -31,9 +24,11 @@ describe('UsuarioModel', () => {
     const result = await usuario.create();
 
     expect(result.login).toBe('usuario01');
+
+    done();
   });
 
-  it('Deve atualizar a senha do usuario01 para www', async () => {
+  it('Deve atualizar a senha do usuario01 para www', async (done) => {
     const usu = {
       email: 'usuario01@email.com',
       login: 'usuario01',
@@ -45,27 +40,36 @@ describe('UsuarioModel', () => {
     const result = await usuario.update();
 
     expect(result.senha).toBe('www');
+
+    done();
   });
 
-  it('Deve recuperar uma lista de usuários', async () => {
+  it('Deve recuperar uma lista de usuários', async (done) => {
     const result = await UsuarioModel.findAll();
 
     expect(result.length).toBeGreaterThan(1);
+
+    done();
   });
 
-  it('Deve encontrar um usuário com ID 20', async () => {
+  it('Deve encontrar um usuário com ID 20', async (done) => {
     const result = await UsuarioModel.findOne('20');
     expect(result).toBe(true);
+
+    done();
   });
 
-  it('Não deve encontrar um usuário com ID 77', async () => {
+  it('Não deve encontrar um usuário com ID 77', async (done) => {
     const result = await UsuarioModel.findOne('77');
     expect(result).toBe(false);
+
+    done();
   });
 
   it('Deve recuperar um usuário com ID 20', async (done) => {
     // const result = await UsuarioModel.findById('20');
     // expect(result[0].login).toBe('dan');
+
     const response = await request(app).get('/usuario/20');
 
     expect(response.status).toBe(200);
